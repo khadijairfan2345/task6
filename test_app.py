59c2fef664661f1b9cba9fabcf20bb7cc281a186
khadijairfan2345 <77497960+khadijairfan2345@users.noreply.github.com>
2023-11-06 11:48:30 +0500

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_home_page(app, client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
